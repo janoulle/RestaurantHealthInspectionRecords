754c5a346485c53ff93a1993e0b316bc259a3c66
Jane Ullah <janeullah@gmail.com>
2017-05-04 02:37:15 -0600

    added caching

--- app/src/main/java/com/janeullah/apps/healthinspectionviewer/services/FetchYelpDataService.java
+++ app/src/main/java/com/janeullah/apps/healthinspectionviewer/services/FetchYelpDataService.java
@@ -2,7 +2,11 @@ package com.janeullah.apps.healthinspectionviewer.services;
 
 import com.janeullah.apps.healthinspectionviewer.interfaces.YelpService;
 
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -25,9 +29,27 @@ public final class FetchYelpDataService {
     }
 
     static{
+        //http://stackoverflow.com/questions/23429046/can-retrofit-with-okhttp-use-cache-data-when-offline
+        Interceptor REWRITE_CACHE_CONTROL_INTERCEPTOR = new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Response originalResponse = chain.proceed(chain.request());
+                String cacheControl = originalResponse.header("Cache-Control");
+                if (cacheControl == null || cacheControl.contains("no-store") || cacheControl.contains("no-cache") ||
+                        cacheControl.contains("must-revalidate") || cacheControl.contains("max-age=0")) {
+                    return originalResponse.newBuilder()
+                            .header("Cache-Control", "public, only-if-cached, max-stale=" + 60 * 60 * 24)
+                            .build();
+                } else {
+                    return originalResponse;
+                }
+            }
+        };
+
         HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
         logging.setLevel(HttpLoggingInterceptor.Level.BODY);
         OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        httpClient.addNetworkInterceptor(REWRITE_CACHE_CONTROL_INTERCEPTOR);
         httpClient.addInterceptor(logging);
 
         RETROFIT = new Retrofit.Builder()
